a8ae059010241ee8ec90336b3f7d15959e40eec7
deivydsantoss <134793784+deivydsantoss@users.noreply.github.com>
2025-10-16 16:40:12 +0000

    start home page

--- tecsan/Views/Home/index.php
+++ tecsan/Views/Home/index.php
@@ -1,12 +1,43 @@
-<div class="info">
-    <div class="title">
-        <h1><?= $information;?></h1>
-    </div>
-    <div class="action_to_login">
-        <a href="<?= BASE_URL.'Login';?>">Login</a>
-    </div>
-</div>
-<hr>
-<div class="about_me">
-    <p><?= $about_me?></p>
-</div>                                                              
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deftec Store</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">DefTec</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                        </button>
+                            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                                  <ul class="navbar-nav">
+                                          <li class="nav-item">
+                                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                                            </li>
+                                                                    <li class="nav-item">
+                                                                              <a class="nav-link" href="#">Features</a>
+                                                                                      </li>
+                                                                                              <li class="nav-item">
+                                                                                                        <a class="nav-link" href="#">Pricing</a>
+                                                                                                                </li>
+                                                                                                                        <li class="nav-item dropdown">
+                                                                                                                                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                                                                                                              Dropdown link
+                                                                                                                                                        </a>
+                                                                                                                                                                  <ul class="dropdown-menu">
+                                                                                                                                                                              <li><a class="dropdown-item" href="#">Action</a></li>
+                                                                                                                                                                                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                                                                                                                                                                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                                                                                                                                                                                                </ul>
+                                                                                                                                                                                                                        </li>
+                                                                                                                                                                                                                              </ul>
+                                                                                                                                                                                                                                  </div>
+                                                                                                                                                                                                                                    </div>
+                                                                                                                                                                                                                                    </nav>
+                                                                                                                                                                      </div>                                                                                                                                                                                                                </nav>
+
+                                                                                                           </body>                                                                                                            </html>                                                              
